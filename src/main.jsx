cd7d9333878d4c7ce5643392d7b8e4d0f9023cb4
Vardensky <sqleer@gmail.com>
2023-12-18 23:20:36 +0300

    Добавил хедер и колонку (нужно сделать)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
+import PopUp from './components/PopUp/PopUp.jsx'
+import Header from './components/Header/Header.jsx'
+import NeedToDo from './components/NeedToDo/NeedToDo.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('wrapper')).render(
   <React.StrictMode>
-    <App />
+    <PopUp />
+		<Header />
+		<NeedToDo />
   </React.StrictMode>,
 )
